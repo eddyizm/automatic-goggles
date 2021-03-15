90b690f71f638d1a7fd60bd441a5bb6af5560a5d
eddyizm <eddyizm@gmail.com>
2021-03-14 23:42:42 -0700

    Added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router'
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StartComponent } from './start/start.component';
+import { MatButtonModule } from '@angular/material/button';
+
+const routes: Routes = [
+  { path: '', component: StartComponent, pathMatch: 'full' },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
